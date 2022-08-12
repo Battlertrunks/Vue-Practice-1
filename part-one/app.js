aa91c181efc01bee725d8153e8b732b211e7573b
Gavin <Gavinszczesniak@gmail.com>
2022-08-12 11:10:51 -0400

    Finished part one, implementing part 2.

--- part-one/app.js
+++ part-one/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish the course and learn Vue!",
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "Master Vue and build amazing apps!",
       vueLink: "https://vuejs.org/",
     };
   },
@@ -9,8 +10,8 @@ const app = Vue.createApp({
     outputGoal() {
       const randomNumber = Math.random();
       if (randomNumber < 0.5) {
-        return "Learn Vue!";
-      } else return "Master Vue!";
+        return this.courseGoalA;
+      } else return this.courseGoalB;
     },
   },
 });
