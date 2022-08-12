1e63a354485fdf9a3f83f8795ab280932990dd09
Gavin <Gavinszczesniak@gmail.com>
2022-08-12 11:42:44 -0400

    Added part two, creating number increment and decrement using events and methods.

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    minus(num) {
+      this.counter -= num;
+    },
+  },
+});
+
+app.mount("#events");
