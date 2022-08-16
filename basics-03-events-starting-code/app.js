f6b9d645d99f4be582e4266a3f03f60ef666d4d8
Gavin <Gavinszczesniak@gmail.com>
2022-08-16 12:12:18 -0400

    Added computedand watch to review the differences and benefits from them on basic-03 section.

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,18 +4,33 @@ const app = Vue.createApp({
       counter: 10,
       name: "",
       confirmedName: "",
+      fullName: "",
     };
   },
+  watch: {
+    name() {
+      this.fullName = this.name + " " + "Szcz";
+    },
+  },
+  computed: {
+    // fullName() {
+    //   console.log("Hello");
+    //   if (this.name) return this.name + " " + "Szczes";
+    // },
+  },
   methods: {
+    resetInput() {
+      this.name = "";
+    },
     confirmInput() {
       this.confirmedName = this.name;
     },
     submitForm() {
       alert("Submitted.");
     },
-    setName(event, lastname) {
-      this.name = event.target.value + " " + lastname;
-    },
+    // setName(event, lastname) {
+    //   this.name = event.target.value;
+    // },
     add(num) {
       this.counter += num;
     },
