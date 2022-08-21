579af7f5c65fac860960b732d1791312aa9aa516
Gavin <Gavinszczesniak@gmail.com>
2022-08-21 17:40:20 -0400

    Took a dive into the reactivity of Vue, working with multiple vue 'apps', when to use refs, how Vue works with the DOM using the virtual DOM, and the Vue App Lifecycle and how it works.

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -10,9 +10,49 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      //this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
     },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
   },
 });
 
-app.mount('#app');
+app.mount("#app");
+
+setTimeout(() => app.unmount(), 3000);
+
+const app2 = Vue.createApp({
+  template: `
+    <p>{{favoriteMeal}}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: "Crab",
+    };
+  },
+});
+
+app2.mount("#app2");
