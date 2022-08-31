1e70897748c45b5d763740cf8da1bb6f580e5851
Gavin <Gavinszczesniak@gmail.com>
2022-08-30 20:29:47 -0400

    initial commit of vue-cli-01 and added an component of app with content to display friends and their fake info.

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li v-for="friend in friends" :key="friend.id">
+        <h3>Name: {{ friend.name }}</h3>
+        <p>phone: {{ friend.phone }}</p>
+        <p>email: {{ friend.email }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "mathew",
+          name: "Mathew Lees",
+          phone: "123 456 7890",
+          email: "Matt@localhost.com",
+        },
+        {
+          id: "ben",
+          name: "Ben Schultz",
+          phone: "123 456 7890",
+          email: "Ben@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
