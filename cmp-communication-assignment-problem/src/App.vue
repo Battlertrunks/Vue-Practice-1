d374e5e3d9ed3b678d18eada835197cba83740f5
Gavin <Gavinszczesniak@gmail.com>
2022-08-31 15:43:37 -0400

    completed an assignment challenge that required parent and child communication, emits, and form to update user information on the page.

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <active-user :username="user.username" :age="user.age"></active-user>
+    <user-data @user-info="updateInfo"></user-data>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "Battlertrunks",
+        age: 24,
+      },
+    };
+  },
+  methods: {
+    updateInfo(username, age) {
+      this.user = {
+        username,
+        age: +age, //converts to an number if a string
+      };
+    },
+  },
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="submitUser">
+    <div>
+      <label for="username">Username: </label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        v-model="enteredUsername"
+      />
+    </div>
+    <div>
+      <label for="age">Age: </label>
+      <input type="number" name="age" id="age" v-model="enteredAge" />
+    </div>
+    <button>Submit User Data</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["user-info"],
+  data() {
+    return {
+      enteredUsername: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitUser() {
+      this.$emit("user-info", this.enteredUsername, this.enteredAge);
+    },
+  },
+};
+</script>
