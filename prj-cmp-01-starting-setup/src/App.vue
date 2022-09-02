60f13c8e9d106b7e695fb2eba00396d01407d6fb
Gavin <Gavinszczesniak@gmail.com>
2022-09-02 18:07:13 -0400

    Moved data into TheResources component and executed a provide and inject into StoredResources. Created AddResource for a future form to add more resources.

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <the-header title="RemeberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
-  components: { StoredResources, TheHeader },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'offical-guide',
-          title: 'Offical Guide',
-          description: 'The offical Vue.js documentation.',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn google...',
-          link: 'https://google.com',
-        },
-      ],
-    };
-  },
+  components: { TheHeader, TheResources },
 };
 </script>
 
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,61 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: { StoredResources, AddResource },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Offical Guide',
+          description: 'The offical Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
