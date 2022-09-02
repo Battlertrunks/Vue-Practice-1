87c7b9ec231b122cc55b2611deab163d8352a3ca
Gavin <Gavinszczesniak@gmail.com>
2022-09-02 16:50:35 -0400

    Inital push and some starting data to output onto the screen.

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      <h2>{{ resource.title }}</h2>
+      <p>{{ resource.description }}</p>
+      <a :href="resource.link">{{ resource.link }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Offical Guide',
+          description: 'The offical Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
