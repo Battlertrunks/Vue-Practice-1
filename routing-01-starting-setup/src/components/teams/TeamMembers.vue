9fa69922b445e8a4152704b5bd5efb20885bc3ab
Gavin <Gavinszczesniak@gmail.com>
2022-09-08 17:53:27 -0400

    Added routes that are dynamic to view specific data and more from filtering based on properties (like id). Lastly, practice using the to attribute with v-bind.

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -16,17 +16,26 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: ['users', 'teams'],
   components: {
-    UserItem
+    UserItem,
   },
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      teamName: '',
+      members: [],
     };
+  },
+  created() {
+    const teamId = this.$route.params.teamId;
+    const selectedTeam = this.teams.find((team) => teamId === team.id);
+    const members = selectedTeam.members;
+    const selectedMembers = [];
+    members.forEach((member) =>
+      selectedMembers.push(this.users.find((user) => user.id === member))
+    );
+    this.members = selectedMembers;
+    this.teamName = selectedTeam.name;
   },
 };
 </script>
