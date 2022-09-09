b861b6b6b9d378595df0280983970b02bffb46aa
Gavin <Gavinszczesniak@gmail.com>
2022-09-09 13:22:49 -0400

    Used the transition tag provided by Vue to make animations when the object appears and is removed. Lastly, can change the name or full name of the transition classes.

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -4,7 +4,9 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <!-- Can add custome css names to use -->
+    <!-- enter-to-class="some-class" enter-active-class="..." can change full name -->
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -91,7 +93,35 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
