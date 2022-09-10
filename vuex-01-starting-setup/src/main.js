9f9326f7f8df10e1fc6bf79d9d14e64a54768971
Gavin <Gavinszczesniak@gmail.com>
2022-09-10 15:36:14 -0400

    Added getters to manipulate the state with restictions and actions to have async code if handling http request and more.

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -17,6 +17,26 @@ const store = createStore({
       state.counter = state.counter + payload.value;
     },
   },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 3;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) return 0;
+      if (finalCounter > 100) return 100;
+      return finalCounter;
+    },
+  },
+  actions: {
+    increment(context) {
+      setTimeout(() => context.commit('increment'), 2000);
+    },
+    increase(context, payload) {
+      console.log(context);
+      context.commit('increase', payload);
+    },
+  },
 });
 
 const app = createApp(App);
