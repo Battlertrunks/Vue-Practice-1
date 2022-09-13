f0744fcac1afdab844b7d053d6f5fa8dee6155b2
Gavin <Gavinszczesniak@gmail.com>
2022-09-12 21:39:53 -0400

    Created form for users to send a request to a coach and hooked it with vuex to store and add the data.

--- main-prj-01-starting-setup/src/store/index.js
+++ main-prj-01-starting-setup/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
+import requestModule from './modules/requests/index.js';
 
 const store = createStore({
-  modules: { coaches: coachesModule },
+  modules: { coaches: coachesModule, requests: requestModule },
   state() {
     return {
       userId: 'c3',
