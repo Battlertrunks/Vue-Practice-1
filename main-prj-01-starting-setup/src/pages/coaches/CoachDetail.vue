64278d049a6f21ddd5e375ba222889ac8dae0d8a
Gavin <Gavinszczesniak@gmail.com>
2022-09-12 23:40:38 -0400

    Added route/page transitions when loading to a new page and implemented a page not found route if the user enters a path that is not recongized.

--- main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -1,30 +1,32 @@
 <template>
-  <section>
-    <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/bour</h3>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Intrested? Reach out now!</h2>
-        <base-button link :to="contactLink">Contact</base-button>
-      </header>
-      <router-view></router-view>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge
-        v-for="area in areas"
-        :key="area.id"
-        :type="area"
-        :title="area"
-      ></base-badge>
-      <p>{{ desc }}</p>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/bour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Intrested? Reach out now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area.id"
+          :type="area"
+          :title="area"
+        ></base-badge>
+        <p>{{ desc }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
